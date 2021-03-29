011ee1775a7874016de724b0383575bf98f28758
Thomas Dierickx <thomas.dierickx3@student.howest.be>
2021-03-29 10:29:02 +0200

    Push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5d7bb00de08a3068bab13ada71fba77f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3677a40b-24ae-4a09-a2ad-afc16826f4dc'
+};
